1272cd85e4971116d493fda365008b7548fb31fc
Shraddha dighe <154206575+shraddhadig@users.noreply.github.com>
2025-08-29 21:28:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const hamburger = document.getElementById('hamburger');
 const menu = document.querySelector('.menu');
+const hamIcon = hamburger.querySelector('.hamburger-icon');
+const crossIcon = hamburger.querySelector('.cross-icon');
 
-hamburger.addEventListener('click', function () {
-    const hamIcon = this.querySelector('.hamburger-icon');
-    const crossIcon = this.querySelector('.cross-icon');
-    if (hamIcon.style.display === "none") {
-        hamIcon.style.display = "inline-block"
-        menu.style.display = "none"
-        crossIcon.style.display = "none"
-    }
-    else {
-        crossIcon.style.display = "inline-block"
-        hamIcon.style.display = "none"
-        menu.style.display = "block"
-    }
+hamburger.addEventListener('click', () => {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+    hamIcon.style.display = "inline-block";
+    crossIcon.style.display = "none";
+  } else {
+    menu.style.display = "block";
+    hamIcon.style.display = "none";
+    crossIcon.style.display = "inline-block";
+  }
 });
+
